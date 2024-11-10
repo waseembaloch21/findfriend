a57b0ce1ff8e4b3d0a7c146b15b42fe4816c3328
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-11-10 11:01:28 +0500

    code update

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -2,30 +2,31 @@ import { connectDB } from "@/lib/db/connectDB";
 import { UserModal } from "@/lib/models/User";
 
 
-export async function GET( request) {
-    await connectDB()
+export async function GET(request) {
+    await connectDB();
     const users = await UserModal.find()
     return Response.json({
-        msg : "Users Fetched Successfully",
+        msg: "Users Fetched Successfully",
         users,
     },
-    {status : 200}
-);
+        { status: 200 }
+    );
 }
 
 export async function POST(request) {
     await connectDB()
-    const obj =await request.json()
+    const obj = await request.json()
     let newUser = new UserModal(obj)
-      await newUser.save();
-     return Response.json({
-        msg : "Users Added Successfully",
+    await newUser.save();
+
+    return Response.json({
+        msg: "Users Added Successfully",
         user: newUser,
-     },
-     { status:201 }
+    },
+        { status: 201 }
     );
 }
- 
-export async function PUT( request) {}
 
-export async function DELETE( request) {}
+export async function PUT(request) { }
+
+export async function DELETE(request) { }
