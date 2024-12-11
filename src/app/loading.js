504f01d3c4079dea5d7f1c0d72e01a4779debc03
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-12-11 16:05:56 +0500

    code done

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,9 @@
 export default function loading() {
     return (
       <div className="flex min-h-screen justify-center items-center flex-row gap-2">
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]" />
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]" />
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]" />
+        <div className="w-6 h-6 rounded-full bg-green-600 animate-bounce [animation-delay:.7s]" />
+        <div className="w-6 h-6 rounded-full bg-red-600 animate-bounce [animation-delay:.3s]" />
+        <div className="w-6 h-6 rounded-full bg-blue-600 animate-bounce [animation-delay:.7s]" />
       </div>
     );
   }
